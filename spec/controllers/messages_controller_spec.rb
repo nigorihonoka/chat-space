db8d669b662b492a694d405d11c73c60bbedfe2c
nigorihonoka <ganache2526@gmail.com>
2020-07-05 14:59:40 +0900

    createアクションのテストコードのexample作成

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -50,6 +50,15 @@ describe MessagesController do
           post :create,
           params: params
         }
+
+        it 'count up message' do
+          expect{ subject }.to change(Message, :count).by(1)
+        end
+
+        it 'redirects to group_messages_path' do
+          subject
+          expect(response).to redirect_to(group_messages_path(group))
+        end
       end
 
       context 'can not save' do
@@ -59,10 +68,23 @@ describe MessagesController do
           post :create,
           params: invalid_params
         }
+
+        it 'does not count up' do
+          expect{ subject }.not_to change(Message, :count)
+        end
+
+        it 'renders index' do
+          subject
+          expect(response).to render_template :index
+        end
       end
     end
 
     context 'not log in' do
+      it 'redirects to new_user_session_path' do
+        post :create, params: params
+        expect(response).to redirect_to(new_user_session_path)
+      end
     end
   end
 end
