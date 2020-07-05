102d68016bbfe56fa390cc3c01fa7ffd41214d57
nigorihonoka <ganache2526@gmail.com>
2020-07-05 12:11:58 +0900

    messageコントローラーテストのテストコードの大枠作成

---
+++ spec/controllers/messages_controller_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe MessagesController do
+  let(:group) { create(:group) }
+  let(:user) { create(:user) }
+
+  describe '#index' do
+    context 'log in' do
+      before do
+        login user
+        get :index, params: { group_id: group.id }
+      end
+    end
+
+    context 'not log in' do
+      before do
+        get :index, params: { group_id: group.id }
+      end
+    end
+  end
+end
