0c648f360e83cf92b7576f510716e6f8904d2553
nigorihonoka <ganache2526@gmail.com>
2020-07-05 14:47:02 +0900

    createアクションのテストコードの大枠

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -33,7 +33,36 @@ describe MessagesController do
       it 'redirects to new_user_session_path' do
         expect(response).to redirect_to(new_user_session_path)
       end
-      
+
+    end
+  end
+
+  describe '#create' do
+    let(:params) { { group_id: group.id, user_id: user.id, message: attributes_for(:message) } }
+
+    context 'log in' do
+      before do
+        login user
+      end
+
+      context 'can save' do
+        subject {
+          post :create,
+          params: params
+        }
+      end
+
+      context 'can not save' do
+        let(:invalid_params) { { group_id: group.id, user_id: user.id, message: attributes_for(:message, content: nil, image: nil) } }
+
+        subject {
+          post :create,
+          params: invalid_params
+        }
+      end
+    end
+
+    context 'not log in' do
     end
   end
 end
