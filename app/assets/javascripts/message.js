1073f57c02c20862eabe00a53724132dd447003e
nigorihonoka <ganache2526@gmail.com>
2020-07-07 19:25:04 +0900

    帰ってきたJSONをdoneメソッドで受け取り、HTMLを形成

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,3 +1,65 @@
 $(function(){
-  console.log("読み込み");
+
+  function buildHTML(message){
+    if ( message.image ) {
+      let html =
+        `<div class="messages">
+          <div class="message">
+            <div class="message__info">
+              <div class="message__info__name">
+                ${ message.user_name }
+              </div>
+              <div class="message__info__date">
+                ${ message.created_at }
+              </div>
+            </div>
+            <div class="message__text">
+              <p class="message__content">
+                ${ message.content }
+              </p>
+              <img class="message__image" src="${ message.image }">
+            </div>
+          </div>
+        </div>`
+      return html;
+    } else {
+      let html =
+      `<div class="messages">
+          <div class="message">
+            <div class="message__info">
+              <div class="message__info__name">
+                ${ message.user_name }
+              </div>
+              <div class="message__info__date">
+                ${ message.created_at }
+              </div>
+            </div>
+            <div class="message__text">
+              <p class="message__content">
+                ${ message.content }
+              </p>
+            </div>
+          </div>
+        </div>`
+      return html;
+    };
+  }
+
+  $(".Form").on("submit", function(e) {
+    e.preventDefault()
+    let formData = new FormData(this);
+    let url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: "json",
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      let html = buildHTML(data);
+      console.log(html)
+    })
+  })
 });
